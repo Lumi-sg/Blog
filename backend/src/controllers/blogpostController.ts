5592555a032b66e848a5b955db35698b488a8688
Lumi <shaicloud@gmail.com>
2023-12-02 20:48:52 -0500

    changed routes to return json

--- backend/src/controllers/blogpostController.ts
+++ backend/src/controllers/blogpostController.ts
@@ -10,27 +10,86 @@ import { User, UserModel } from "../models/User";
 
 export const blogpost_list_get = asyncHandler(
 	async (req: express.Request, res: express.Response) => {
-		const blogposts = await BlogPostModel.find()
-			.sort({ timePosted: -1 })
-			.populate("author");
+		try {
+			const blogposts = await BlogPostModel.find()
+				.sort({ timePosted: -1 })
+				.populate("author");
 
-		res.render("posts", { blogposts });
+			res.json({ blogposts });
+		} catch (error: any) {
+			res.status(500).json({ message: error.message });
+		}
 	}
 );
 
 export const blogpost_get = asyncHandler(
 	async (req: express.Request, res: express.Response) => {
-		const blogpost = await BlogPostModel.findById(req.params.id).populate("author");
-		// .populate("comments");
-		if (blogpost) {
-			const encodedContent = blogpost.content;
-			const decodedContent = he.decode(encodedContent);
-			blogpost.content = decodedContent;
+		try {
+			const blogpost = await BlogPostModel.findById(req.params.id).populate(
+				"author"
+			);
+			if (blogpost) {
+				const encodedContent = blogpost.content;
+				const decodedContent = he.decode(encodedContent);
+				blogpost.content = decodedContent;
+			}
+			res.json({ blogpost });
+		} catch (error: any) {
+			res.status(500).json({ message: error.message });
 		}
-		res.render("post", { blogpost, errors: [] });
 	}
 );
 
+export const comment_form_post = [
+	body("username")
+		.trim()
+		.isLength({ min: 2, max: 25 })
+		.escape()
+		.withMessage("Name must be between 2 and 25 characters long."),
+	body("commentContent")
+		.trim()
+		.isLength({ min: 2, max: 144 })
+		.escape()
+		.withMessage("Comment must be between 2 and 144 characters long."),
+
+	asyncHandler(async (req: express.Request, res: express.Response) => {
+		try {
+			const errors = validationResult(req);
+
+			if (!errors.isEmpty()) {
+				res.status(400).json({ errors: errors.array() });
+			} else {
+				const { username, commentContent, postId } = req.body;
+
+				console.log("Received Post ID from form:", postId);
+
+				const comment: Comment = {
+					commentContent,
+					username,
+					timePosted: moment().format("MMMM D, YYYY - h:mm A"),
+				};
+
+				const blogpost = await BlogPostModel.findById(postId);
+				if (blogpost) {
+					blogpost.comments.push(comment);
+					await blogpost.save();
+					console.log(`Successfully created comment by ${username}`);
+					res.status(200).json({
+						message: "Comment created successfully",
+						comment,
+					});
+				} else {
+					res.status(404).json({ error: "Post not found" });
+				}
+			}
+		} catch (error: any) {
+			console.error("Error during comment form post:", error);
+			res.status(500).json({ message: error.message });
+		}
+	}),
+];
+
+//backend-frontend stuff -------------------------------------------
 export const blogpost_form_get = asyncHandler(
 	async (req: express.Request, res: express.Response) => {
 		dotenv.config();
@@ -81,51 +140,6 @@ export const blogpost_form_post = [
 			}
 		} catch (error) {
 			console.error("Error during blogpost form post:", error);
-			res.status(500).send("Internal Server Error");
-		}
-	}),
-];
-
-export const comment_form_post = [
-	body("username")
-		.trim()
-		.isLength({ min: 2, max: 25 })
-		.escape()
-		.withMessage("Name must be between 2 and 25 characters long."),
-	body("commentContent")
-		.trim()
-		.isLength({ min: 2, max: 144 })
-		.escape()
-		.withMessage("Comment must be between 2 and 144 characters long."),
-
-	asyncHandler(async (req: express.Request, res: express.Response) => {
-		try {
-			const errors = validationResult(req);
-
-			if (!errors.isEmpty()) {
-				res.render(`/posts`, { errors: errors.array() });
-				console.log(errors.array());
-			} else {
-				const { username, commentContent, postId } = req.body;
-
-				console.log("Received Post ID from form:", postId);
-
-				const comment: Comment = {
-					commentContent,
-					username,
-					timePosted: moment().format("MMMM D, YYYY - h:mm A"),
-				};
-
-				const blogpost = await BlogPostModel.findById(postId);
-				if (blogpost) {
-					blogpost.comments.push(comment);
-					await blogpost.save();
-					console.log(`Successfully created comment by ${username}`);
-					res.redirect(`/post/${postId}`);
-				}
-			}
-		} catch (error) {
-			console.error("Error during comment form post:", error);
 			res.status(500).send("Internal Server Error");
 		}
 	}),
