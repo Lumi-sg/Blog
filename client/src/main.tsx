45eab39c7d7efda99ed6e03a778d9e15f4e42c4c
Lumi <shaicloud@gmail.com>
2023-12-03 11:18:12 -0500

    basic routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Navigate, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Blogpost from "./Components/Blogpost/Blogpost.tsx";
+import ErrorPage from "./Components/ErrorPage/ErrorPage.tsx";
+import Index from "./Components/Index/Index.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Navigate to="/index" />,
+	},
+
+	{
+		path: "/index",
+		element: <Index />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: "/post/:id",
+		element: <Blogpost />,
+		errorElement: <ErrorPage />,
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
