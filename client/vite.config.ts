c6e0f717b82d8fc2f5520af373e64e37761c3777
Lumi <shaicloud@gmail.com>
2023-12-03 21:14:17 -0500

    minor changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,8 +4,5 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/Blog",
-	build: {
-		target: "esnext",
-	},
+	base: "/Blog/",
 });
